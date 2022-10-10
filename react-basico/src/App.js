1573f08b6ab50a9a43466380de6005550fd20a6a
portella0803 <lucas.portell03@gmail.com>
2022-10-10 11:37:50 -0300

    iniciando_react

---
+++ react-basico/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Comentario from './components/Comentario';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Meu Projeto</h1>
+      <Comentario />
+      <Comentario />
+      <Comentario />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-basico/src/components/Comentario.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import './Comentario.css';
+
+//JSX
+const Comentario = () => (
+    <div className="Comentario">
+        <div>João</div>
+        <div>Olá, tudo bem?</div>
+    </div>
+);
+
+export default Comentario;
