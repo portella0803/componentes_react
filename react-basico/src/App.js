425ed01314247e3c4b5143781850c64eeda74c60
portella0803 <lucas.portell03@gmail.com>
2022-10-10 13:22:21 -0300

    estado_da_aplicação

--- react-basico/src/App.js
+++ react-basico/src/App.js
@@ -9,12 +9,14 @@ class App extends Component {
   state = {
     comentarios: [
       {
+        id: 1234,
         nome: 'João',
         email: 'joao@email.com',
         data: new Date(2020, 3, 19),
         mensagem: 'Olá, tudo bem?'
       },
       {
+        id: 5678,
         nome: 'Maria',
         email: 'maria@email.com',
         data: new Date(2020, 3, 21),
@@ -30,7 +32,7 @@ class App extends Component {
 
       {this.state.comentarios.map((comentario, indice) =>(
         <Comentario 
-        key = {indice}
+        key = {comentario.id}
         nome={comentario.nome} 
         email={comentario.email} 
         data={comentario.data} 
